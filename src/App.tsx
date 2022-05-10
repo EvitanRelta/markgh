c0f44ddf4fb4ac6eb04ab49587845e1624209f38
EvitanRelta <shauntanzongzhi@gmail.com>
2022-05-10 18:36:21 +0800

    Add basic Quill editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import TextEditor from './components/TextEditor'
+
 export default function App() {
     return (
         <div id='app'>
+            <TextEditor />
         </div>
     )
 }
---
+++ src/components/TextEditor.tsx
@@ -0,0 +1,30 @@
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
+import { useCallback } from 'react'
+
+const TOOLBAR_OPTIONS = [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ align: [] }]
+]
+
+export default function TextEditor() {
+    const wrapperRef = useCallback((wrapper: HTMLDivElement) => {
+        if (!wrapper) return
+        wrapper.innerHTML = ''
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+
+        new Quill(editor, {
+            theme: 'snow',
+            modules: { toolbar: TOOLBAR_OPTIONS }
+        })
+    }, [])
+
+    return (
+        <div id='container' ref={wrapperRef}></div>
+    )
+}
