e252e057871b25f27580bc1b4c71f937529d9b43
EvitanRelta <shauntanzongzhi@gmail.com>
2022-05-17 21:09:52 +0800

    Fix 'Turndown' not recognising 'align' attribute

--- src/helperFunctions/htmlToMarkdown.ts
+++ src/helperFunctions/htmlToMarkdown.ts
@@ -5,5 +5,18 @@ import { gfm } from 'turndown-plugin-gfm'
 export default function htmlToMarkdown(html: string) {
     const turndownService = new TurndownService({ headingStyle: 'atx' })
     turndownService.use(gfm)
+    turndownService.addRule('align', {
+        filter: (node, options) => {
+            console.log([node])
+            const alignment = node.getAttribute('align')?.toLocaleLowerCase() || 'left'
+            return ['right', 'center', 'justify'].includes(alignment)
+        },
+        replacement: (content, node, options) => {
+            const tag = node.nodeName.toLowerCase()
+            //@ts-expect-error
+            const alignment = node.getAttribute('align')
+            return `<${tag} align="${alignment}">${content}</${tag}>`
+        }
+    })
     return turndownService.turndown(html)
 }
