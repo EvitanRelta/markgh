61464468be7c3b6f3e6a9a85ea2a858b7424eb05
EvitanRelta <shauntanzongzhi@gmail.com>
2022-05-18 15:57:52 +0800

    Fix links not converting to HTML
    Wrapping markdown hyperlinks (ie. `[TEXT](URL)`) in a HTML tag will
    render as literal `[TEXT](URL)` string. Thus, it needs to be converted
    to HTML too.

--- src/helperFunctions/htmlToMarkdown.ts
+++ src/helperFunctions/htmlToMarkdown.ts
@@ -9,12 +9,26 @@ export default function htmlToMarkdown(html: string) {
         .replaceAll(/(?<!<pre[^<]*>[^<]*)(?<=>)\s/g, '&nbsp;')
         .replaceAll(/(?<!<pre[^<]*>[^<]*)\s(?=<)/g, '&nbsp;')
     const escapedAmp = spaceToNbsp.replaceAll(/(?<!<pre[^<]*>[^<]*)&/g, '&amp;')
-        
+
     const turndownService = new TurndownService({
         headingStyle: 'atx',
         codeBlockStyle: 'fenced'
     })
     turndownService.use(gfm)
+    turndownService.addRule('htmlLinks', {
+        filter: (node, options) => {
+            let parentElement = node.parentElement
+            while (parentElement && parentElement.id !== 'turndown-root') {
+                if (parentElement.className !== '')
+                    return true
+                parentElement = parentElement.parentElement
+            }
+            return false
+        },
+        replacement: (content, node, options) => {
+            return `<a href="${(node as HTMLElement).getAttribute('href')}">${content}</a>`
+        }
+    })
     turndownService.addRule('align', {
         filter: (node, options) => {
             const classNames: string[] = Array.from(node.classList)
@@ -27,6 +41,7 @@ export default function htmlToMarkdown(html: string) {
             const alignment = classNames
                 .find(className => className.includes('ql-align-'))
                 ?.replace('ql-align-', '')
+            console.log(content)
             return `<${tag} align="${alignment}">${content}</${tag}>`
         }
     })
