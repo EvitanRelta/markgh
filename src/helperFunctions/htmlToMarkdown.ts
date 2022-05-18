4baa34bb7ae790403204507c52bc6cd058889850
EvitanRelta <shauntanzongzhi@gmail.com>
2022-05-18 19:38:14 +0800

    Replace align replacement to allow markdown-in-html\
    Replacing: <h1 align="center"><a href="www.google.com">Google</a></h1>
    With:
    <h1 align="center">
    
    [Google](www.google.com)
    
    </h1>

--- src/helperFunctions/htmlToMarkdown.ts
+++ src/helperFunctions/htmlToMarkdown.ts
@@ -4,27 +4,13 @@ export default function htmlToMarkdown(html: string) {
     const spaceToNbsp = html
         .replaceAll(/\n(?=<\/pre>)/g, '')
         .replaceAll(/(?<!<pre[^<]*>[^<]*)(?<=\s)\s/g, '&nbsp;')
-        .replaceAll(/(?<!<pre[^<]*>[^<]*)(?<=>)\s/g, '&nbsp;')
+        .replaceAll(/(?<!<pre[^<]*>[^<]*)(?<!<\/[^>]+>)(?<=>)\s/g, '&nbsp;')
         .replaceAll(/(?<!<pre[^<]*>[^<]*)\s(?=<)/g, '&nbsp;')
     const escapedAmp = spaceToNbsp.replaceAll(/(?<!<pre[^<]*>[^<]*)&/g, '&amp;')
 
     const turndownService = new TurndownService({
         headingStyle: 'atx',
         codeBlockStyle: 'fenced'
-    })
-    turndownService.addRule('htmlLinks', {
-        filter: (node, options) => {
-            let parentElement = node.parentElement
-            while (parentElement && parentElement.id !== 'turndown-root') {
-                if (parentElement.className !== '')
-                    return true
-                parentElement = parentElement.parentElement
-            }
-            return false
-        },
-        replacement: (content, node, options) => {
-            return `<a href="${(node as HTMLElement).getAttribute('href')}">${content}</a>`
-        }
     })
     turndownService.addRule('align', {
         filter: (node, options) => {
@@ -39,7 +25,7 @@ export default function htmlToMarkdown(html: string) {
                 .find(className => className.includes('ql-align-'))
                 ?.replace('ql-align-', '')
             console.log(content)
-            return `<${tag} align="${alignment}">${content}</${tag}>`
+            return `<${tag} align="${alignment}">\n\n${content}\n\n</${tag}>`
         }
     })
     turndownService.addRule('codeblock', {
