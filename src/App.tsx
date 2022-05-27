0f0bbddc33b5b2ef84aea271b2f4fc48cd4f4c69
EvitanRelta <shauntanzongzhi@gmail.com>
2022-05-27 23:45:09 +0800

    Revert 'App.tsx' & 'TextEditor.tsx', removing all temp code

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,6 @@ export default function App() {
     return (
         <div id='app'>
             <TextEditor />
-            <hr />
-            <pre id='raw-html' />
-            <hr />
-            <pre id='markdown' />
-            <hr />
-            <div id='rendered-markdown' />
         </div>
     )
 }
--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,7 +1,6 @@
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
-import { useCallback, useState } from 'react'
-import { markdownToHtml, toMarkdown } from '../converterFunctions'
+import { useCallback } from 'react'
 
 const TOOLBAR_OPTIONS = [
     ['bold', 'italic', 'underline', 'strike', 'code'],
@@ -9,35 +8,19 @@ const TOOLBAR_OPTIONS = [
 
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
     [{ list: 'ordered' }, { list: 'bullet' }],
-    [{ align: [] }],
+    [{ align: [] }]
 ]
 
 export default function TextEditor() {
-    const [quill, setQuill] = useState<Quill | null>(null)
     const wrapperRef = useCallback((wrapper: HTMLDivElement) => {
         if (!wrapper) return
         wrapper.innerHTML = ''
         const editor = document.createElement('div')
         wrapper.append(editor)
-        const quill = new Quill(editor, {
+
+        new Quill(editor, {
             theme: 'snow',
             modules: { toolbar: TOOLBAR_OPTIONS }
-        })
-
-        setQuill(quill)
-        //@ts-ignore
-        window.quill = quill
-        quill.on('text-change', () => {
-            const rawHtml = quill.root
-            const markdown = toMarkdown(rawHtml)
-            const renderedMarkdown = markdownToHtml(markdown)
-
-            // @ts-ignore
-            document.getElementById('raw-html').innerText = rawHtml.innerHTML
-            // @ts-ignore
-            document.getElementById('markdown').innerText = markdown
-            // @ts-ignore
-            document.getElementById('rendered-markdown').innerHTML = renderedMarkdown
         })
     }, [])
 
