5f729b9d63812ceb1995a8bb83e4143e618723e0
swxk19 <s.wxk19@gmail.com>
2022-05-30 16:13:23 +0800

    Change 'Show Markdown' button to fixed position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,27 @@ import Button from '@mui/material/Button'
 
 const Footer = ({ onClick, showMarkdown }) => (
     <footer className='footer'>
-        <p style={{
-            marginTop: '100px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
+        <div 
+        // style={{
+        //     marginTop: '100px',
+        //     display: 'flex',
+        //     justifyContent: 'center',
+        //     alignItems: 'center'
+        // }}
+        style = {{
+            margin: 0,
+            top: 745,
+            right: 40,
+            bottom: 40,
+            left: '20',
+            position: 'fixed',
         }}>
             {
                 showMarkdown
                     ? <Button onClick={onClick} variant="outlined">Hide Markdown</Button>
                     : <Button color='primary' onClick={onClick} variant="contained">Show Markdown</Button>
             }
-        </p>
+        </div>
     </footer>
 )
 
