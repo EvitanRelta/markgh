1e80011039f66c6bfbff4d5e7681a4eda63bb633
EvitanRelta <shauntanzongzhi@gmail.com>
2022-05-31 22:58:44 +0800

    Refactor 'toMarkdownpost-processing

--- src/converterFunctions/toMarkdown.ts
+++ src/converterFunctions/toMarkdown.ts
@@ -43,13 +43,39 @@ function replaceOnlyNonCodeOrCodeBlock(markdown: string, replacementFn: (markdow
 }
 
 function postProcessHtml(markdown: string) {
-    const postProcess = (markdown: string) => markdown
+    type StrReplacement = (str: string) => string
+
+    // Assumes that there's no more than 2 '&nbsp;' in a row, as HTML naturally 
+    // (and probably) will not do that.
+    // eg. '[TEXT]&nbsp;&nbsp;[TEXT]' -> '[TEXT]&nbsp; [TEXT]'
+    const unescapeDoubleNbsp: StrReplacement = x => x
         .replace(/(?<=&nbsp;)&nbsp;/g, ' ')
+
+    // eg. '[TEXT]&nbsp;[TEXT]' -> '[TEXT] [TEXT]'
+    const unescapeUnnecessaryNbsp: StrReplacement = x => x
         .replace(/(?<!\s)&nbsp;(?!\s)/g, ' ')
+
+    // eg. '[TEXT]&nbsp; &nbsp; &nbsp;[TEXT]' -> '[TEXT] &nbsp; &nbsp; [TEXT]'
+    const reduceOddNumOfNbsp: StrReplacement = x => x
         .replace(/(?<!\s)((&nbsp; )+)&nbsp;(?!\s)/g, ' $1')
+
+    // For better readability.
+    // eg. '[TEXT]&nbsp; &nbsp; [TEXT]' -> '[TEXT] &nbsp;&nbsp; [TEXT]'
+    const avoidNbspBesideWords: StrReplacement = x => x
         .replace(/(?<!\s)&nbsp; &nbsp; ((&nbsp; )*)/g, ' &nbsp;&nbsp; $1')
+
+    // '&lt;&amp;' -> '\<\&'
+    const htmlEscapeToBackslashEscape: StrReplacement = x => x
         .replaceAll('&lt;', '\\<')
         .replaceAll('&amp;', '\\&')
+
+    const postProcess: StrReplacement = markdown => [
+        unescapeDoubleNbsp,
+        unescapeUnnecessaryNbsp,
+        reduceOddNumOfNbsp,
+        avoidNbspBesideWords,
+        htmlEscapeToBackslashEscape
+    ].reduce((str, fn) => fn(str), markdown)
 
     return replaceOnlyNonCodeOrCodeBlock(markdown, postProcess)
 }
