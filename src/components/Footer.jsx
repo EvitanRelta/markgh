3ee3de8e276f48bef001c401d751711957531c52
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-01 20:31:48 +0800

    Refactor 'Footer.jsx' indentations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,19 @@ import Button from '@mui/material/Button'
 
 const Footer = ({ onClick, showMarkdown, theme }) => (
     <footer className='footer'>
-        <div 
-        style = {{
-            margin: 0,
-            top: 745,
-            right: 40,
-            bottom: 40,
-            left: '20',
-            position: 'fixed',
-        }}>
+        <div
+            style={{
+                margin: 0,
+                top: 745,
+                right: 40,
+                bottom: 40,
+                left: '20',
+                position: 'fixed',
+            }}>
             {
                 showMarkdown
-                    ? <Button onClick={onClick} variant="outlined" 
-                    style = {{backgroundColor: theme === 'light' ? 'white' : 'black', minWidth: 169.42}}>Hide Markdown</Button>
+                    ? <Button onClick={onClick} variant="outlined"
+                        style={{ backgroundColor: theme === 'light' ? 'white' : 'black', minWidth: 169.42 }}>Hide Markdown</Button>
                     : <Button color='primary' onClick={onClick} variant="contained">Show Markdown</Button>
             }
         </div>
