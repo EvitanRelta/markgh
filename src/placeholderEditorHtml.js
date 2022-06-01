fc5808c6ccf8d709617215fd40a4901c794386bb
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-01 10:13:40 +0800

    Replace 'placeholderEditorHtml' code-block to match markdown output

--- src/placeholderEditorHtml.js
+++ src/placeholderEditorHtml.js
@@ -1,9 +1,9 @@
 export default `<h1 class="ql-align-center">MarkGH&nbsp;<img src="https://github.com/EvitanRelta/testing-grounds/raw/master/assets/logo.png" height="30"></h1><p>Online markdown editor for&nbsp;&nbsp;<img src="https://github.com/EvitanRelta/testing-grounds/raw/master/assets/github_logo.png" height="25"></p><ul><li>Avoid writing <strong><em>raw markdown</em></strong></li><li>Edit it like a <strong><em>Word document</em></strong></li></ul><h2>Hide this HTML-Markdown mess:</h2><pre class="ql-syntax" spellcheck="false"><span class="hljs-tag">&lt;<span class="hljs-name">h1</span> <span class="hljs-attr">align</span>=<span class="hljs-string">"center"</span>&gt;</span>
-MarkGH <span class="hljs-tag">&lt;<span class="hljs-name">img</span> <span class="hljs-attr">src</span>=<span class="hljs-string">"https://github.com/EvitanRelta/testing-grounds/raw/master/assets/logo.png"</span> <span class="hljs-attr">height</span>=<span class="hljs-string">"30"</span> /&gt;</span>
+  MarkGH <span class="hljs-tag">&lt;<span class="hljs-name">img</span> <span class="hljs-attr">src</span>=<span class="hljs-string">"https://github.com/EvitanRelta/testing-grounds/raw/master/assets/logo.png"</span> <span class="hljs-attr">height</span>=<span class="hljs-string">"30"</span> /&gt;</span>
 <span class="hljs-tag">&lt;/<span class="hljs-name">h1</span>&gt;</span>
 
 Online markdown editor for<span class="hljs-symbol">&amp;nbsp;</span> <span class="hljs-tag">&lt;<span class="hljs-name">img</span> <span class="hljs-attr">src</span>=<span class="hljs-string">"https://github.com/EvitanRelta/testing-grounds/raw/master/assets/github_logo.png"</span> <span class="hljs-attr">height</span>=<span class="hljs-string">"25"</span> /&gt;</span>
 
-*   Avoid writing raw markdown
-*   Edit it like a Word document
+*   Avoid writing **_raw markdown_**
+*   Edit it like a **_Word document_**
 </pre>`
