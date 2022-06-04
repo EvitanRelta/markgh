c13e62f7f0aa11798393f4c2c64abce8b83af294
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-04 23:55:40 +0800

    Add 'italic' option

--- src/components/EditorToolbar.tsx
+++ src/components/EditorToolbar.tsx
@@ -1,6 +1,7 @@
 import { Editor } from '@tiptap/react'
 import addUrlImage from './EditorOptions/toolbarFunctions/addUrlImage'
 import bold from './EditorOptions/toolbarFunctions/bold'
+import italic from './EditorOptions/toolbarFunctions/italic'
 
 
 interface Props {
@@ -10,6 +11,7 @@ interface Props {
 export default ({ editor }: Props) => (
     <div>
         <button onClick={bold(editor)}>Bold</button>
+        <button onClick={italic(editor)}>Italic</button>
         <button onClick={addUrlImage(editor)}>Add image via URL</button>
     </div>
 )
