9c20e9dd8e59b20ad51779ef21ddcdff2c4dbf8d
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-05 15:38:44 +0800

    Link specific comment in 'SizedImage' github issue

--- src/components/Editor/extensions/SizedImage.ts
+++ src/components/Editor/extensions/SizedImage.ts
@@ -1,6 +1,6 @@
 import Image from '@tiptap/extension-image'
 
-// Based on https://github.com/ueberdosis/tiptap/issues/1365
+// Based on https://github.com/ueberdosis/tiptap/issues/1365#issuecomment-853799239
 export default Image.extend({
     addAttributes() {
         return {
