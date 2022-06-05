b22245f554f079f8a55a582e32ef1f6359f9e633
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-05 15:28:51 +0800

    Add 'strikethrough' option

--- src/components/Editor/EditorToolbar.tsx
+++ src/components/Editor/EditorToolbar.tsx
@@ -2,6 +2,7 @@ import { Editor } from '@tiptap/react'
 import addUrlImage from './toolbarFunctions/addUrlImage'
 import bold from './toolbarFunctions/bold'
 import italic from './toolbarFunctions/italic'
+import strikethrough from './toolbarFunctions/strikethrough'
 import underline from './toolbarFunctions/underline'
 
 
@@ -14,6 +15,7 @@ export default ({ editor }: Props) => (
         <button onClick={bold(editor)}>Bold</button>
         <button onClick={italic(editor)}>Italic</button>
         <button onClick={underline(editor)}>Underline</button>
+        <button onClick={strikethrough(editor)}>StrikeThrough</button>
         <button onClick={addUrlImage(editor)}>Add image via URL</button>
     </div>
 )
