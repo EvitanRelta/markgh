fd03d8050b2b008dafc48b17bd7064b6e5f46182
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-05 15:33:46 +0800

    Add 'code' option

--- src/components/Editor/EditorToolbar.tsx
+++ src/components/Editor/EditorToolbar.tsx
@@ -1,6 +1,7 @@
 import { Editor } from '@tiptap/react'
 import addUrlImage from './toolbarFunctions/addUrlImage'
 import bold from './toolbarFunctions/bold'
+import code from './toolbarFunctions/code'
 import italic from './toolbarFunctions/italic'
 import strikethrough from './toolbarFunctions/strikethrough'
 import underline from './toolbarFunctions/underline'
@@ -16,6 +17,7 @@ export default ({ editor }: Props) => (
         <button onClick={italic(editor)}>Italic</button>
         <button onClick={underline(editor)}>Underline</button>
         <button onClick={strikethrough(editor)}>StrikeThrough</button>
+        <button onClick={code(editor)}>Code</button>
         <button onClick={addUrlImage(editor)}>Add image via URL</button>
     </div>
 )
