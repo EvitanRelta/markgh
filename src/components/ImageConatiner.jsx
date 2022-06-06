3360e625214772a954757ee32c2cec48e97c3add
swxk19 <s.wxk19@gmail.com>
2022-06-07 05:33:42 +0800

    Fix bug with 'Delete all images'

--- src/components/ImageConatiner.jsx
+++ src/components/ImageConatiner.jsx
@@ -52,7 +52,8 @@ const ImageConatiner = () => {
 
     //Clears db
     const deleteAllImages = async () => {
-        for (let i = 0; i < images.length; i++) {
+        for (let i = 0; i <= images[images.length -1].id; i++) {
+            console.log(i)
             db.images.delete(i);
         }
         updateImagesFromDb();
