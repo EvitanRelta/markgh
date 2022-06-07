d3f9725fd290a41b463a9362892279e2ca11f5a0
swxk19 <s.wxk19@gmail.com>
2022-06-07 17:44:11 +0800

    Add code comments in 'App'

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,27 @@ import Header from './components/Header'
 import toMarkdown from './converterFunctions/toMarkdown'
 
 export default function App(): ReactElement {
+
+    //var for controlling whether to show markdown
     const [showMarkdown, setShowMarkdown] = useState(false)
+
+    //var for theme control
     const [mode, setMode] = useState('light')
+    
+    //var for setting file title
     const [title, setTitle] = useState('')
+
+    //var for to contain markdown text
     const [mdText, setMdText] = useState('')
+
+
     const [quill, setQuill] = useState<Quill | null>(null)
 
     //var for 'Last edited on'
     const [lastEditedOn, setLastEditedOn] = useState(localStorage["lastEditedOn"])
 
+
+    //Defining theme colors
     const darkTheme = createTheme({
         palette: {
             mode: 'dark',
@@ -29,33 +41,41 @@ export default function App(): ReactElement {
         },
     })
 
+    //Check selectedTheme
     const selectedTheme = mode === "dark" ? darkTheme : lightTheme
 
+    //Executes when user uploads a .md or .txt file
     const onUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        //Allowed file types
         const allowedFileTypes = ['txt', 'md']
+
+        //Retrieving file from event
         const target = e.target as HTMLInputElement
-
         let file = (target.files![0])
-
         const reader = new FileReader()
 
+        //Check file type
         const getFileType = (fileName: string) => {
             return (fileName.split('.').pop()!.toLowerCase())
         }
 
+        //Alert for wrong file type
         if (!allowedFileTypes.includes(getFileType(file.name))) {
             alert('Invalid file type! (.txt or .md only)')
             return
         }
 
+        //Reading file and allocating to state
         reader.readAsText(file)
         reader.onload = () => {
             setMdText(reader.result as string)
         }
     }
 
+
     const getMarkdownText = () => toMarkdown(document.getElementsByClassName('ql-editor')[0] as HTMLElement)
 
+    //Updates markdown text when prompted to show it
     useEffect(() => {
         if (!showMarkdown) return
         if (document.getElementsByClassName('ql-editor')[0] === undefined) return
@@ -63,6 +83,7 @@ export default function App(): ReactElement {
         setMdText(getMarkdownText())
     }, [showMarkdown])
 
+    //Updates markdown text when text is changed in quill
     useEffect(() => {
         if (quill === null) return
 
@@ -71,7 +92,10 @@ export default function App(): ReactElement {
         })  
     }, [quill])
 
+    //Updates 'Last Edited On' in local storage when text is changed in quill
     useEffect(() => {
+
+        //Formatting time as text
         const date = new Date()
         const n = date.toDateString() 
         var t = date.toLocaleTimeString()
@@ -79,6 +103,7 @@ export default function App(): ReactElement {
         var dateShort = n.substring(4, n.length - 5)
         var dateTime = dateShort + " " + timeShort
 
+        //Updates 'Last Edited On' in localStorage and state when text in quill is changed
         if (quill == null) return
         quill.on('text-change', () => {
             setLastEditedOn(dateTime)
