6bcdfc8ced5f7b547f3a8c7cfadafa2ac144e793
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-08 15:25:56 +0800

    Replace 'align' rule to convert 'align' attr.

--- src/converterFunctions/turndownPlugins/align.ts
+++ src/converterFunctions/turndownPlugins/align.ts
@@ -1,27 +1,12 @@
 import { Plugin } from 'turndown'
-import indent from '../indent'
 import turndownHtmlOnly from '../turndownHtmlOnly'
 
 const align: Plugin = (service) => {
     service.addRule('align', {
-        filter: (node, options) => {
-            const classNames = Array.from(node.classList)
-            return classNames.some((className) =>
-                className.includes('ql-align-')
-            )
-        },
+        filter: (node, options) => node.getAttribute('align') !== null,
         replacement: (content, node, options) => {
             const element = node as HTMLElement
-            const tag = element.nodeName.toLowerCase()
-            const alignment = element.className.match(
-                /(?<=(?<!\S)ql-align-)\S+/
-            )?.[0]
-            const innerMarkdown = turndownHtmlOnly.turndown(element)
-            return (
-                `<${tag} align="${alignment}">\n` +
-                indent(innerMarkdown) +
-                `\n</${tag}>\n\n`
-            )
+            return turndownHtmlOnly.turndown(element.outerHTML)
         },
     })
 }
