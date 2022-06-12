bfc0cffeaad8eca8ce4dd8351f6fde1354dd924d
swxk19 <s.wxk19@gmail.com>
2022-06-12 16:55:36 +0800

    Add copy to clipboard button

--- src/components/Body/MarkdownTextContainer.tsx
+++ src/components/Body/MarkdownTextContainer.tsx
@@ -1,20 +1,42 @@
+import { useState } from 'react'
+import CopyClipboardButton from './CopyClipboardButton'
+
 type Props = {
     mdText: string
 }
 
 const MarkdownTextContainer = ({ mdText }: Props) => {
+    const [isHovering, setIsHovering] = useState(false)
+
     return (
-        <pre
+        <div
             style={{
-                border: '1px solid #d0cccc',
                 width: '50%',
+                border: '1px solid #d0cccc',
                 padding: '20px',
+                paddingTop: 0,
                 margin: '10px',
                 overflowX: 'auto',
             }}
+            onMouseLeave={() => setIsHovering(false)}
+            onMouseEnter={() => setIsHovering(true)}
         >
-            {mdText}
-        </pre>
+            {isHovering && (
+                <div
+                    style={{
+                        marginLeft: 637.5,
+                        marginTop: 6,
+                        border: '1px solid #d0cccc',
+                        minWidth: 35,
+                        padding: 0,
+                        borderRadius: 5,
+                    }}
+                >
+                    <CopyClipboardButton />
+                </div>
+            )}
+            <pre style={{}}>{mdText}</pre>
+        </div>
     )
 }
 
