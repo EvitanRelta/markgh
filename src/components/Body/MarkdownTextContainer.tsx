babc85cca6682d76c941d3818347c504a17d1fcb
swxk19 <s.wxk19@gmail.com>
2022-06-12 18:10:26 +0800

    Implement copy to clipboard function & stylings (repositiong & animations)

--- src/components/Body/MarkdownTextContainer.tsx
+++ src/components/Body/MarkdownTextContainer.tsx
@@ -1,3 +1,4 @@
+import Button from '@mui/material/Button'
 import { useState } from 'react'
 import CopyClipboardButton from './CopyClipboardButton'
 
@@ -8,34 +9,46 @@ type Props = {
 const MarkdownTextContainer = ({ mdText }: Props) => {
     const [isHovering, setIsHovering] = useState(false)
 
+    const [showCopiedPopup, setShowCopiedPopup] = useState(false)
+
+    const onCopy = () => {
+        navigator.clipboard.writeText(mdText)
+        popUpCopied()
+    }
+
+    const popUpCopied = () => {
+        setShowCopiedPopup(true)
+        setTimeout(() => setShowCopiedPopup(false), 1000)
+    }
+
     return (
         <div
             style={{
                 width: '50%',
                 border: '1px solid #d0cccc',
                 padding: '20px',
-                paddingTop: 0,
+                paddingTop: 2,
+                paddingRight: 8,
                 margin: '10px',
                 overflowX: 'auto',
+                justifyContent: 'space-between',
+                display: 'flex',
             }}
             onMouseLeave={() => setIsHovering(false)}
             onMouseEnter={() => setIsHovering(true)}
         >
+            <pre style={{ marginTop: 15 }}>{mdText}</pre>
             {isHovering && (
-                <div
-                    style={{
-                        marginLeft: 637.5,
-                        marginTop: 6,
-                        border: '1px solid #d0cccc',
-                        minWidth: 35,
-                        padding: 0,
-                        borderRadius: 5,
-                    }}
-                >
-                    <CopyClipboardButton />
+                <div>
+                    {showCopiedPopup ? (
+                        <Button sx={{ marginTop: 1, marginRight: 0.5 }}>
+                            Copied
+                        </Button>
+                    ) : (
+                        <CopyClipboardButton onClick={onCopy} />
+                    )}
                 </div>
             )}
-            <pre style={{}}>{mdText}</pre>
         </div>
     )
 }
