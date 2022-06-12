de429a431043ee8521b01d63f58975a7bbf70d32
swxk19 <s.wxk19@gmail.com>
2022-06-12 18:10:26 +0800

    Implement copy to clipboard function & stylings (repositiong & animations)

--- src/components/Body/CopyClipboardButton.js
+++ src/components/Body/CopyClipboardButton.js
@@ -1,10 +1,19 @@
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import { IconButton } from '@mui/material'
 
-const CopyClipboardButton = () => {
+const CopyClipboardButton = ({ onClick }) => {
     return (
-        <div>
+        <div
+            style={{
+                marginTop: 6,
+                border: '1px solid #d0cccc',
+                padding: 0,
+                borderRadius: 5,
+                maxHeight: 37,
+            }}
+        >
             <IconButton
+                onClick={onClick}
                 color='inherit'
                 size='small'
                 sx={{ transform: 'scaleX(-1) scale(0.8)' }}
