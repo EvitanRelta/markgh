18289d7fe727d4308209eb7883e01795dc7117e7
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-17 17:56:37 +0800

    Replace tab with spaces in codeblock

--- src/components/Editor/extensions/TabKey.ts
+++ src/components/Editor/extensions/TabKey.ts
@@ -4,10 +4,20 @@ export const TabKey = Extension.create({
     name: 'tabKey',
 
     addKeyboardShortcuts() {
+        const indentSpaces = 2
+
         return {
             Tab: () => {
                 const editor = this.editor
-                return editor.commands.insertContent('\t')
+                if (!editor.isActive('codeBlock')) return editor.commands.insertContent('\t')
+
+                const $anchor = editor.state.selection.$anchor
+                const textBeforeCursor = $anchor.parent.textBetween(0, $anchor.parentOffset)
+                const textFromStartOfLine = textBeforeCursor.match(/(?<=^|\n)[^\n]*$/)?.[0] || ''
+                const charsFromStartOfLine = textFromStartOfLine.length
+                const spaces = indentSpaces - (charsFromStartOfLine % indentSpaces)
+
+                return editor.commands.insertContent(' '.repeat(spaces))
             },
         }
     },
