279236ad712730b9bec7bdc1d5d73a53fbac6c3f
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-17 19:40:50 +0800

    Add 'Shift-Tab' unindenting in codeblocks

--- src/components/Editor/extensions/TabKey.ts
+++ src/components/Editor/extensions/TabKey.ts
@@ -19,6 +19,26 @@ export const TabKey = Extension.create({
 
                 return editor.commands.insertContent(' '.repeat(spaces))
             },
+            'Shift-Tab': () => {
+                const editor = this.editor
+                if (!editor.isActive('codeBlock')) return true
+
+                const $anchor = editor.state.selection.$anchor
+                const textBeforeCursor = $anchor.parent.textBetween(0, $anchor.parentOffset)
+                const textFromStartOfLine = textBeforeCursor.match(/(?<=^|\n)[^\n]*$/)?.[0] || ''
+                const currentIndent = /^ */.exec(textFromStartOfLine)?.[0].length || 0
+
+                const canBeUnindented = currentIndent >= indentSpaces
+                if (!canBeUnindented) return true
+
+                const spacesToDelete = currentIndent % indentSpaces || indentSpaces
+                const charsFromStartOfLine = textFromStartOfLine.length
+                const startOfLinePos = $anchor.pos - charsFromStartOfLine
+                return editor.commands.deleteRange({
+                    from: startOfLinePos,
+                    to: startOfLinePos + spacesToDelete,
+                })
+            },
         }
     },
 })
