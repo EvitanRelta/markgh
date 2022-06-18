740e398edf9489cb602c5134bc33181257e229f0
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-18 23:42:58 +0800

    Move cursor to end of indent when Shift-Tab in codeblock

--- src/components/Editor/extensions/TabKey.ts
+++ src/components/Editor/extensions/TabKey.ts
@@ -26,10 +26,27 @@ export const TabKey = Extension.create({
                 const $anchor = editor.state.selection.$anchor
                 const textBeforeCursor = $anchor.parent.textBetween(0, $anchor.parentOffset)
                 const textFromStartOfLine = textBeforeCursor.match(/(?<=^|\n)[^\n]*$/)?.[0] || ''
-                const currentIndent = /^ */.exec(textFromStartOfLine)?.[0].length || 0
 
+                const textAfterCursor = $anchor.parent.textContent.slice($anchor.parentOffset)
+                const textFromCursorTillEOL = textAfterCursor.match(/^[^\n]*(?=\n|$)/)?.[0] || ''
+
+                const currentLine = textFromStartOfLine + textFromCursorTillEOL
+                const currentIndent = /^ */.exec(currentLine)?.[0].length || 0
                 const canBeUnindented = currentIndent >= indentSpaces
                 if (!canBeUnindented) return true
+
+                const cursorIsInIndent =
+                    /^ *$/.test(textFromStartOfLine) &&
+                    textFromCursorTillEOL !== '' &&
+                    textFromCursorTillEOL[0] === ' '
+
+                if (cursorIsInIndent) {
+                    // Moves cursor to the end of the indent
+                    // eg. "    |  TEXT", where | is the cursor
+                    // becomes: "      |TEXT"
+                    const offset = /^ +/.exec(textFromCursorTillEOL)?.[0].length || 0
+                    editor.commands.setTextSelection($anchor.pos + offset)
+                }
 
                 const spacesToDelete = currentIndent % indentSpaces || indentSpaces
                 const charsFromStartOfLine = textFromStartOfLine.length
