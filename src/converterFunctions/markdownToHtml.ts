9f03085aabd8540efe35fab0e0969988bcbda335
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-19 16:19:48 +0800

    Enable auto-linker for 'markdownToHtml'
    For converting URLs in the markdown into hyperlinks.

--- src/converterFunctions/markdownToHtml.ts
+++ src/converterFunctions/markdownToHtml.ts
@@ -1,6 +1,6 @@
 import MarkdownIt from 'markdown-it'
 
 export default (markdown: string) => {
-    const markdownit = new MarkdownIt({ html: true })
+    const markdownit = new MarkdownIt({ html: true, linkify: true })
     return markdownit.render(markdown)
 }
