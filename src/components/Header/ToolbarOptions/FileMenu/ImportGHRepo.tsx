1f48f53fd04900d7d80f48f64767b25c95cbdd1a
swxk19 <s.wxk19@gmail.com>
2022-06-20 21:52:22 +0800

    Close popover after logging in for private repo

--- src/components/Header/ToolbarOptions/FileMenu/ImportGHRepo.tsx
+++ src/components/Header/ToolbarOptions/FileMenu/ImportGHRepo.tsx
@@ -86,7 +86,7 @@ const ImportGHRepo = ({ setAnchor, menuOpen, ghToken, onLogin }: Props) => {
                     setShowError(true)
                     return
                 }
-                onLogin(githubProvider)
+                onLogin(githubProvider).then(() => setAnchor(null))
             })
     }
 
