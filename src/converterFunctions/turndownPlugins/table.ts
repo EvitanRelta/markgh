3a13f9f5ed344a9b0b2b8c615cdfe64def35e486
EvitanRelta <shauntanzongzhi@gmail.com>
2022-06-22 20:18:36 +0800

    Check if table elements has attributes not allowed in markdown

--- src/converterFunctions/turndownPlugins/table.ts
+++ src/converterFunctions/turndownPlugins/table.ts
@@ -11,16 +11,58 @@ const getTableRowMarkdownHOF = (longestTextLens: number[]) => (tr: HTMLTableRowE
     )
 }
 
+const canBeInMarkdown = (element: HTMLTableElement) => {
+    if (element.tHead === null) return false
+    if (element.tHead.hasAttribute('height') || element.tBodies[0].hasAttribute('height'))
+        return false
+
+    const rowHasHeight = (row: HTMLTableRowElement) => row.hasAttribute('height')
+    if (
+        Array.from(element.tHead.rows).some(rowHasHeight) ||
+        Array.from(element.tBodies[0].rows).some(rowHasHeight)
+    )
+        return false
+
+    const getCells = (row: HTMLTableRowElement) => Array.from(row.cells)
+    const cells = [
+        Array.from(element.tHead.rows[0].cells),
+        ...Array.from(element.tBodies[0].rows).map(getCells),
+    ]
+
+    const cellHasSize = (cell: HTMLTableCellElement) =>
+        cell.hasAttribute('width') || cell.hasAttribute('height')
+    const cellHasInvalidAlign = (cell: HTMLTableCellElement) =>
+        ![null, 'left', 'center', 'right'].includes(cell.getAttribute('align'))
+    if (
+        cells.some((row) => row.some(cellHasSize)) ||
+        cells.some((row) => row.some(cellHasInvalidAlign))
+    )
+        return false
+
+    // Ensures each column has same alignment
+    const firstRowAligns = cells[0].map((cell) => cell.getAttribute('align'))
+    for (let row = 1; row < cells.length; row++)
+        for (let col = 0; col < cells[0].length; col++)
+            if (cells[row][col].getAttribute('align') !== firstRowAligns[col]) return false
+
+    return true
+}
+
+// Presumes 'element.tHead' only has 1 row,
+// and 'element.tBodies' only has 1 tbody element.
+// Allows the attributes 'align' & 'height' on thead, tbody, tr.
+// Allows 'align', 'width, 'height' on th, td.
 const table: Plugin = (service) => {
     service.addRule('table', {
         filter: 'table',
         replacement: (content, node, options) => {
             const element = node as HTMLTableElement
-            if (element.tHead === null) return turndownHtmlOnly.turndown(element.outerHTML)
+            if (!canBeInMarkdown(element)) return turndownHtmlOnly.turndown(element)
 
+            const tHead = element.tHead as HTMLTableSectionElement
             const getCellLen = (cell: HTMLTableCellElement) => cell.innerText.length
             const textLens = [
-                Array.from(element.tHead.rows[0].cells).map(getCellLen),
+                Array.from(tHead.rows[0].cells).map(getCellLen),
                 ...Array.from(element.tBodies[0].rows).map((row) =>
                     Array.from(row.cells).map(getCellLen)
                 ),
@@ -36,7 +78,7 @@ const table: Plugin = (service) => {
 
             const getTableRowMarkdown = getTableRowMarkdownHOF(longestTextLens)
 
-            const header = getTableRowMarkdown(element.tHead.rows[0])
+            const header = getTableRowMarkdown(tHead.rows[0])
             const body = Array.from(element.tBodies[0].rows).map(getTableRowMarkdown).join('\n')
             const separatorRow =
                 '| ' +
