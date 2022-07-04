90898d53145c75abe302fb7b0ad7e836e19c6ec2
swxk19 <s.wxk19@gmail.com>
2022-07-05 00:15:17 +0800

    (WIP) Refactor inline stylings in 'Body' to use MUI 'styled'

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, styled } from '@mui/material'
 import { useAppSelector } from '../../store/hooks'
 import { EditorToolbar } from '../Editor/EditorToolbar'
 import { TextEditor } from '../Editor/TextEditor'
@@ -13,27 +13,27 @@ export const Body = ({ showMarkdown, onTextChange }: Props) => {
     const editorWidth = showMarkdown ? '50%' : '100%'
     const editor = useAppSelector((state) => state.editor.editor)
 
+    const StyledContentContainer = styled(Box)({
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        display: 'flex',
+    })
+
+    const StyledEditorContianer = styled(Box)({
+        margin: '10px',
+        width: editorWidth,
+        height: 'stretch',
+    })
+
     return (
         <>
             <EditorToolbar editor={editor} />
-            <Box
-                style={{
-                    justifyContent: 'center',
-                    alignItems: 'stretch',
-                    display: 'flex',
-                }}
-            >
-                <Box
-                    style={{
-                        margin: '10px',
-                        width: editorWidth,
-                        height: 'stretch',
-                    }}
-                >
+            <StyledContentContainer>
+                <StyledEditorContianer>
                     <TextEditor onTextChange={onTextChange} />
-                </Box>
+                </StyledEditorContianer>
                 {showMarkdown && <MarkdownTextContainer />}
-            </Box>
+            </StyledContentContainer>
         </>
     )
 }
--- src/components/Body/CopyClipboardButton.tsx
+++ src/components/Body/CopyClipboardButton.tsx
@@ -1,29 +1,28 @@
 import { ContentCopy as ContentCopyIcon } from '@mui/icons-material'
-import { IconButton } from '@mui/material'
+import { Box, IconButton, styled } from '@mui/material'
 
 interface Props {
     onClick: React.MouseEventHandler<HTMLButtonElement>
+    isHovering: boolean
 }
 
-export const CopyClipboardButton = ({ onClick }: Props) => {
+export const CopyClipboardButton = ({ onClick, isHovering }: Props) => {
+    const StyledClipboardButtonContainer = styled(Box)({
+        marginTop: 6,
+        padding: 2,
+        paddingTop: 6,
+        borderRadius: 5,
+        display: 'inline',
+    })
+    const StyledIconButton = styled(IconButton)({
+        transform: 'scaleX(-1) scale(0.8)',
+    })
+
     return (
-        <div
-            style={{
-                marginTop: 6,
-                padding: 2,
-                paddingTop: 6,
-                borderRadius: 5,
-                display: 'inline',
-            }}
-        >
-            <IconButton
-                onClick={onClick}
-                color='inherit'
-                size='small'
-                sx={{ transform: 'scaleX(-1) scale(0.8)' }}
-            >
+        <StyledClipboardButtonContainer>
+            <StyledIconButton onClick={onClick} size='small'>
                 <ContentCopyIcon />
-            </IconButton>
-        </div>
+            </StyledIconButton>
+        </StyledClipboardButtonContainer>
     )
 }
