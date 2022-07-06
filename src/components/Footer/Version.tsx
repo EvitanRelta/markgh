7940e0b1edfc8bc08e88a2c59849de31b06eed12
swxk19 <s.wxk19@gmail.com>
2022-07-06 14:57:35 +0800

    Remove inline stylings for 'Version'

--- src/components/Footer/Version.tsx
+++ src/components/Footer/Version.tsx
@@ -1,18 +1,20 @@
-import { Box } from '@mui/material'
+import { Box, styled } from '@mui/material'
 import packageInfo from '../../../package.json'
 
-export const Version = () => (
-    <Box
-        style={{
-            margin: 0,
-            left: 40,
-            bottom: 40,
-            position: 'fixed',
-            color: 'gray',
-        }}
-    >
-        <strong>
-            <em>version: {packageInfo.version}</em>
-        </strong>
-    </Box>
-)
+export const Version = () => {
+    const StyledVersionContainer = styled(Box)({
+        margin: 0,
+        left: 40,
+        bottom: 40,
+        position: 'fixed',
+        color: 'gray',
+    })
+
+    return (
+        <StyledVersionContainer>
+            <strong>
+                <em>version: {packageInfo.version}</em>
+            </strong>
+        </StyledVersionContainer>
+    )
+}
