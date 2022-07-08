d7ff6b19cf8af0a3630a6b0a640020423ef4f383
EvitanRelta <shauntanzongzhi@gmail.com>
2022-07-08 17:36:14 +0800

    Remove artifact comments

--- src/scripts/helpers/generateChangeLogText.ts
+++ src/scripts/helpers/generateChangeLogText.ts
@@ -46,28 +46,3 @@ export const generateChangeLogText = (tagChanges: TagChanges): string => {
         `**Full Changelog**: https://github.com/${repoOwner}/${repoName}/compare/${tagChanges.previousTag}...${tagChanges.tag}`
     )
 }
-
-/*
-## What's Changed
-* Site UI elements by @swxk19 in https://github.com/EvitanRelta/markgh/pull/1
-* Change markdown button to fixed position by @swxk19 in https://github.com/EvitanRelta/markgh/pull/2
-* Convert most files to TypeScript by @swxk19 in https://github.com/EvitanRelta/markgh/pull/12
-* Storage features by @swxk19 in https://github.com/EvitanRelta/markgh/pull/24
-* Toolbar for File Options by @swxk19 in https://github.com/EvitanRelta/markgh/pull/25
-* Organise and clean repo by @swxk19 in https://github.com/EvitanRelta/markgh/pull/26
-* Add feature to copy markdown to clipboard by @swxk19 in https://github.com/EvitanRelta/markgh/pull/27
-* Polish UI for EditorToolbar by @swxk19 in https://github.com/EvitanRelta/markgh/pull/31
-* (WIP) Add git-clone feature  by @swxk19 in https://github.com/EvitanRelta/markgh/pull/40
-* Add support for cloning private repos by @swxk19 in https://github.com/EvitanRelta/markgh/pull/55
-* Add feature to allow for editor text to persist by @swxk19 in https://github.com/EvitanRelta/markgh/pull/59
-* Fix persistent text warnings  by @swxk19 in https://github.com/EvitanRelta/markgh/pull/60
-* Refactor elements to use MUI by @swxk19 in https://github.com/EvitanRelta/markgh/pull/62
-* Add support for SEO in Google search by @swxk19 in https://github.com/EvitanRelta/markgh/pull/64
-* Polishes for UI (Issue #46) by @swxk19 in https://github.com/EvitanRelta/markgh/pull/68
-* Change method of importing READMEs from repos by @swxk19 in https://github.com/EvitanRelta/markgh/pull/72
-* Change site logo & title by @swxk19 in https://github.com/EvitanRelta/markgh/pull/78
-* Resize 'OK' button  by @swxk19 in https://github.com/EvitanRelta/markgh/pull/79
-
-
-**Full Changelog**: https://github.com/EvitanRelta/markgh/commits/v0.2.0
-*/
