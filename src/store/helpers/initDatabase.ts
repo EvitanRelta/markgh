103b1b0e32a9c673382e2fe83e7c0e212840ee2f
EvitanRelta <shauntanzongzhi@gmail.com>
2022-07-11 13:48:53 +0800

    Add database-check during database initialisation
    Close #139

--- src/store/helpers/initDatabase.ts
+++ src/store/helpers/initDatabase.ts
@@ -34,4 +34,31 @@ export type EditorDBInstance = InstanceType<typeof EditorDB>
 
 const database: EditorDBInstance = new EditorDB()
 
+const resetDatabase = async () => {
+    console.error('Resetting database...')
+    await database.delete()
+    window.location.reload()
+}
+
+database.on('ready', async () => {
+    const tableNames = database.tables.map((table) => table.name)
+    for (const tableName of tableNames) {
+        const table = database.table(tableName)
+        const records = await table.toArray()
+        const primaryKey = table.schema.primKey.name
+        const properties = table.schema.indexes.map((x) => x.name).concat(primaryKey)
+
+        const isProperty = (key: string) => properties.includes(key)
+        const hasAllProperties = (record: any) =>
+            Object.keys(record).every(isProperty) &&
+            Object.keys(record).length === properties.length
+
+        for (const record of records) {
+            if (!hasAllProperties(record)) await resetDatabase()
+        }
+    }
+})
+
+database.open().catch(resetDatabase)
+
 export { database }
