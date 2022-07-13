9ba9b616438accd4cdfd603d3ee345404630ede2
swxk19 <s.wxk19@gmail.com>
2022-07-14 07:06:25 +0800

    Add content framework for 'PushGH' dialog box

--- src/components/Header/ToolbarOptions/ExportMenu/PushGH.tsx
+++ src/components/Header/ToolbarOptions/ExportMenu/PushGH.tsx
@@ -1,5 +1,5 @@
 import FS from '@isomorphic-git/lightning-fs'
-import { Dialog, MenuItem } from '@mui/material'
+import { Box, Button, Dialog, DialogContent, MenuItem, TextField } from '@mui/material'
 import { useState } from 'react'
 
 const fs = new FS('fs')
@@ -9,8 +9,63 @@ window.Buffer = window.Buffer || require('buffer').Buffer
 
 export const PushGH = () => {
     const [showDialog, setShowDialog] = useState(false)
+    const [link, setLink] = useState<string>('')
+    const [errorMessage, setErrorMessage] = useState<string>('')
+    const [showError, setShowError] = useState<boolean>(false)
+    const [showLoading, setShowLoading] = useState<boolean>(false)
 
-    const pushInputDialog = <Dialog open={showDialog} onClose={() => setShowDialog(false)}></Dialog>
+    const getRepo = async () => {}
+
+    const pushInputDialog = (
+        <Dialog open={showDialog} onClose={() => setShowDialog(false)} fullWidth>
+            <DialogContent sx={{ height: 500, alignItems: 'center' }}>
+                <Box sx={{ marginLeft: 10 }}>
+                    <h1>Push to GitHub</h1>
+
+                    <h5>Insert Respository Link</h5>
+                    <p>
+                        <TextField
+                            error={showError}
+                            type='text'
+                            size='small'
+                            label={'Repository Link'}
+                            placeholder={'https://github.com/user/project'}
+                            InputLabelProps={{ shrink: true }}
+                            onChange={(e) => {
+                                setLink(e.target.value)
+                                setShowError(false)
+                                setShowLoading(false)
+                            }}
+                            onKeyPress={(ev) => {
+                                if (ev.key === 'Enter') {
+                                    ev.preventDefault()
+                                    getRepo()
+                                }
+                            }}
+                            helperText={
+                                showError
+                                    ? errorMessage
+                                    : 'Please login if you are accessing a private repo'
+                            }
+                        />
+                    </p>
+                    <p>
+                        <Button>Push</Button>
+                    </p>
+                    <p>Pull Request created. View it here</p>
+                    <p>
+                        <Button>Delete Branch</Button>
+                    </p>
+                    <Box sx={{ marginRight: 10, fontSize: 12, marginTop: -1.5 }}>
+                        <h3>How it's done</h3>
+                        <p>1. A branch is created on your repository</p>
+                        <p>2. A pull request of your exported README is created</p>
+                        <p>3. You can review the pull request and merge it if you're satisfied</p>
+                    </Box>
+                </Box>
+            </DialogContent>
+        </Dialog>
+    )
 
     return (
         <>
