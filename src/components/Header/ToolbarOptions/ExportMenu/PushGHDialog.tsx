8179eeded83c5e0dbdce138d1aa26a51d8fe6fd9
swxk19 <s.wxk19@gmail.com>
2022-07-15 02:32:11 +0800

    Add stylings for link input in 'PushGHDialog'

--- src/components/Header/ToolbarOptions/ExportMenu/PushGHDialog.tsx
+++ src/components/Header/ToolbarOptions/ExportMenu/PushGHDialog.tsx
@@ -11,55 +11,60 @@ export const PushGHDialog = ({ setShowDialog }: Props) => {
     const [errorMessage, setErrorMessage] = useState<string>('')
     const [showLoading, setShowLoading] = useState<boolean>(false)
     const [showError, setShowError] = useState<boolean>(false)
+    const [showFinished, setShowFinished] = useState<boolean>(false)
 
     const getRepo = async () => {}
 
     return (
         <Dialog open={true} onClose={() => setShowDialog(false)} fullWidth>
-            <DialogContent sx={{ height: 500, alignItems: 'center' }}>
+            <DialogContent sx={{ alignItems: 'center' }}>
                 <Box sx={{ marginLeft: 10 }}>
                     <h1>Push to GitHub</h1>
+                    <Box sx={{ minHeight: 130 }}>
+                        <h5 style={{ marginTop: 10, marginLeft: 10 }}>Insert Respository Link</h5>
 
-                    <h5>Insert Respository Link</h5>
-
-                    <TextField
-                        error={showError}
-                        type='text'
-                        size='small'
-                        label={'Repository Link'}
-                        placeholder={'https://github.com/user/project'}
-                        InputLabelProps={{ shrink: true }}
-                        onChange={(e) => {
-                            setLink(e.target.value)
-                            setShowError(false)
-                            setShowLoading(false)
-                        }}
-                        onKeyPress={(ev) => {
-                            if (ev.key === 'Enter') {
-                                ev.preventDefault()
-                                getRepo()
+                        <TextField
+                            error={showError}
+                            type='text'
+                            size='small'
+                            label={'Repository Link'}
+                            placeholder={'https://github.com/user/project'}
+                            InputLabelProps={{ shrink: true }}
+                            onChange={(e) => {
+                                setLink(e.target.value)
+                                setShowError(false)
+                                setShowLoading(false)
+                            }}
+                            onKeyPress={(ev) => {
+                                if (ev.key === 'Enter') {
+                                    ev.preventDefault()
+                                    getRepo()
+                                }
+                            }}
+                            helperText={
+                                showError
+                                    ? errorMessage
+                                    : 'Please login if you are accessing a private repo'
                             }
-                        }}
-                        helperText={
-                            showError
-                                ? errorMessage
-                                : 'Please login if you are accessing a private repo'
-                        }
-                    />
+                        />
 
-                    <Button
-                        sx={{ display: 'block' }}
-                        onClick={() => updateGitHubReadme(link, localStorage['ghToken'])}
-                    >
-                        Push
-                    </Button>
+                        <Button
+                            sx={{ marginLeft: 1 }}
+                            onClick={() => {
+                                setShowLoading(true)
+                                updateGitHubReadme(link, localStorage['ghToken'])
+                            }}
+                        >
+                            Push
+                        </Button>
 
-                    <p>Pull Request created. View it here</p>
+                        {showFinished && <p>Pull Request created. View it here</p>}
 
-                    <Button>Delete Branch</Button>
+                        {/* <Button>Delete Branch</Button> */}
+                    </Box>
 
                     <Box sx={{ marginRight: 10, fontSize: 12, marginTop: -1.5 }}>
-                        <h3>How it's done</h3>
+                        <h3 style={{ marginLeft: 10 }}>How it's done</h3>
                         <p>1. A branch is created on your repository</p>
                         <p>2. A pull request of your exported README is created</p>
                         <p>3. You can review the pull request and merge it if you're satisfied</p>
