e5409585d0cf589a545d9bca74b67766019cbedb
EvitanRelta <shauntanzongzhi@gmail.com>
2022-07-16 19:18:34 +0800

    Refactor 'getTagChanges' to be more readable

--- src/scripts/helpers/changeLogHelpers/getTagChanges.ts
+++ src/scripts/helpers/changeLogHelpers/getTagChanges.ts
@@ -5,24 +5,15 @@ import { memoGetUserName } from './getUserName'
 import { getIssueData, getPullRequestData } from './githubDataGetters'
 import { getClosedIssuesFromBodyText, hasIssueClosingKeywords } from './issueClosingKeywordParsers'
 import { scrapeCommitData } from './scrapeCommitData'
-import type { ClosedIssueData, PullRequestData, TagChanges } from './types'
+import type {
+    ClosedIssueData,
+    PullRequestData,
+    ScrappedCommitDataWUserName,
+    TagChanges,
+} from './types'
 
 type IssueNumber = number
 type IntermediateClosedIssues = Record<IssueNumber, SetOptional<ClosedIssueData, 'issue'>>
-
-const prMergeRegex = /^Merge pull request #([0-9]+) /
-const isPullRequestMerge = (commitTitle: string) => prMergeRegex.test(commitTitle)
-const getPRNumber = (commitTitle: string) => {
-    const match = prMergeRegex.exec(commitTitle)
-    if (!match) return null
-    return parseInt(match[1])
-}
-const getIssuesClosedByPR = (pr: PullRequestData) => {
-    const prDoesntHaveBody = pr.body === null
-    if (prDoesntHaveBody) return []
-
-    return getClosedIssuesFromBodyText(pr.body!)
-}
 
 export const getTagChanges = async (tag: string): Promise<TagChanges> => {
     logMsg('Scrapping local commits from Git...')
@@ -38,18 +29,27 @@ export const getTagChanges = async (tag: string): Promise<TagChanges> => {
     )
 
     const closedIssues: IntermediateClosedIssues = {}
-
-    logMsg('Getting PRs...')
-    const mergedPullRequests = await Promise.all(
-        prMerges.map((commit) => getPRNumber(commit.title)!).map(getPullRequestData)
-    )
-
     const initDataIfNotExists = (issueNumber: number) => {
         if (!closedIssues[issueNumber])
-            closedIssues[issueNumber] = {
-                closedBy: { commits: [], pullRequests: [] },
-            }
+            closedIssues[issueNumber] = { closedBy: { commits: [], pullRequests: [] } }
     }
+
+    logMsg('Fetching pull requests from Github...')
+    const prNumbers = prMerges.map((commit) => getPRNumber(commit.title)!)
+    const mergedPullRequests: PullRequestData[] = await Promise.all(
+        prNumbers.map(getPullRequestData)
+    )
+
+    logMsg('Fetching usernames from Github...')
+    const addingUserNamePromise = issueClosingCommits.map(async (commit) => ({
+        ...commit,
+        authorUserName: await memoGetUserName(commit),
+    }))
+    const issueClosingCommitsWUserName: ScrappedCommitDataWUserName[] = await Promise.all(
+        addingUserNamePromise
+    )
+
+    // Group the merged PRs by closed-issue number.
     for (const pr of mergedPullRequests) {
         for (const issueNumber of getIssuesClosedByPR(pr)) {
             initDataIfNotExists(issueNumber)
@@ -57,24 +57,22 @@ export const getTagChanges = async (tag: string): Promise<TagChanges> => {
         }
     }
 
-    logMsg('Getting Commits/Usernames...')
-    for (const commit of issueClosingCommits) {
-        const authorUserName = await memoGetUserName(commit)
+    // Group the commits by closed-issue number.
+    for (const commit of issueClosingCommitsWUserName) {
         for (const issueNumber of getClosedIssuesFromBodyText(commit.body)) {
             initDataIfNotExists(issueNumber)
-            closedIssues[issueNumber].closedBy.commits.push({
-                ...commit,
-                authorUserName,
-            })
+            closedIssues[issueNumber].closedBy.commits.push(commit)
         }
     }
 
     const entries = Object.entries(closedIssues)
     const issueNumbers = entries.map((x) => parseInt(x[0]))
 
+    logMsg('Fetching issues from Github...')
     // Gets all issue data together asynchronously.
-    logMsg('Getting Issues...')
     const issues = await Promise.all(issueNumbers.map(getIssueData))
+
+    // Insert fetched issue-data into each entry.
     entries.forEach(([_, data], i) => (data.issue = issues[i]))
 
     return {
@@ -83,4 +81,19 @@ export const getTagChanges = async (tag: string): Promise<TagChanges> => {
         closedIssues: Object.values(closedIssues) as ClosedIssueData[],
         mergedPullRequests,
     }
+}
+
+const prMergeRegex = /^Merge pull request #([0-9]+) /
+const isPullRequestMerge = (commitTitle: string) => prMergeRegex.test(commitTitle)
+const getPRNumber = (commitTitle: string) => {
+    const match = prMergeRegex.exec(commitTitle)
+    if (!match) return null
+    return parseInt(match[1])
+}
+
+const getIssuesClosedByPR = (pullRequest: PullRequestData) => {
+    const hasNoBody = pullRequest.body === null
+    if (hasNoBody) return []
+
+    return getClosedIssuesFromBodyText(pullRequest.body!)
 }
