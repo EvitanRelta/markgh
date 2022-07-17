9314e9507b2cd099238e440c49a3bbde2820c27c
EvitanRelta <shauntanzongzhi@gmail.com>
2022-07-17 22:45:12 +0800

    Switch to HTML-syntax if bold/italic/strikethough has space at ends

--- src/converterFunctions/turndownPlugins/plugins.ts
+++ src/converterFunctions/turndownPlugins/plugins.ts
@@ -4,6 +4,7 @@ import { codeBlocks } from './codeBlocks'
 import { emptyParagraph } from './emptyParagraph'
 import { lineBreak } from './lineBreak'
 import { resizedImage } from './resizedImage'
+import { startOrEndWithSpace } from './startOrEndWithSpace'
 import { strikethrough } from './strikethrough'
 import { subscript } from './subscript'
 import { superscript } from './superscript'
@@ -20,4 +21,7 @@ export const plugins = [
     underline,
     subscript,
     superscript,
+
+    // Must be last, so that it's highest priority.
+    startOrEndWithSpace,
 ]
---
+++ src/converterFunctions/turndownPlugins/startOrEndWithSpace.ts
@@ -0,0 +1,20 @@
+import { Plugin } from 'turndown'
+import { turndownHtmlOnly } from '../helpers/turndownHtmlOnly'
+
+export const startOrEndWithSpace: Plugin = (service) => {
+    service.addRule('startOrEndWithSpace', {
+        filter: (node, options) => {
+            // Bold, italic & strikethroughs.
+            const affectedTags = ['STRONG', 'B', 'EM', 'I', 'DEL', 'S']
+            if (!affectedTags.includes(node.nodeName)) return false
+
+            // '&nbsp;' characters have already been escaped during preprocess.
+            const startOrEndWithSpace = /(^(&nbsp;| )|(&nbsp;| )$)/.test(node.innerText)
+            return startOrEndWithSpace
+        },
+        replacement: (content, node, options) => {
+            const element = node as HTMLElement
+            return turndownHtmlOnly.turndown(element.outerHTML)
+        },
+    })
+}
