83fa89002e5690b7eba7ccf77cc43a374636de69
EvitanRelta <shauntanzongzhi@gmail.com>
2022-07-18 01:19:31 +0800

    Make title width grow based on title length

--- src/components/Header/TitleInput.tsx
+++ src/components/Header/TitleInput.tsx
@@ -1,4 +1,5 @@
 import { Input, styled } from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
 import { setFileTitle } from '../../store/dataSlice'
 import { useAppDispatch, useAppSelector } from '../../store/hooks'
 
@@ -21,10 +22,35 @@ const StyledInput = styled(Input)({
     },
 })
 
+const getFont = (element: Element) => {
+    const getCssStyle = (element: Element, prop: string) =>
+        window.getComputedStyle(element, null).getPropertyValue(prop)
+    const fontWeight = getCssStyle(element, 'font-weight') || 'normal'
+    const fontSize = getCssStyle(element, 'font-size') || '16px'
+    const fontFamily = getCssStyle(element, 'font-family') || 'Times New Roman'
+    return `${fontWeight} ${fontSize} ${fontFamily}`
+}
+
+const context = document.createElement('canvas').getContext('2d')!
+const getTextWidth = (text: string, font: string) => {
+    context.font = font
+    const { width } = context.measureText(text)
+    return width
+}
+
 export const TitleInput = () => {
     const dispatch = useAppDispatch()
     const fileTitle = useAppSelector((state) => state.data.fileTitle)
     const theme = useAppSelector((state) => state.theme)
+    const inputRef = useRef<HTMLInputElement>()
+    const [width, setWidth] = useState<number | undefined>(undefined)
+
+    useEffect(() => {
+        const newWidth = !inputRef.current
+            ? undefined
+            : getTextWidth(fileTitle, getFont(inputRef.current)) * 1.05 + 25
+        setWidth(newWidth)
+    }, [setWidth, inputRef, fileTitle])
 
     //vars for theme control
     const themeColor = theme === 'dark' ? '#181414' : 'white'
@@ -35,8 +61,13 @@ export const TitleInput = () => {
             type='text'
             placeholder='Untitled Document'
             value={fileTitle}
+            inputRef={inputRef}
             onChange={(e) => dispatch(setFileTitle(e.target.value))}
-            sx={{ backgroundColor: themeColor, color: textColor }}
+            sx={{
+                backgroundColor: themeColor,
+                color: textColor,
+                width,
+            }}
         />
     )
 }
