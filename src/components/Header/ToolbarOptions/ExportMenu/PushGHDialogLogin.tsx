5124e86a660ed7c59ca320d2fe0f23f8e2d829c3
swxk19 <s.wxk19@gmail.com>
2022-07-19 00:42:56 +0800

    Add login function to 'PushGHDialog'

--- src/components/Header/ToolbarOptions/ExportMenu/PushGHDialogLogin.tsx
+++ src/components/Header/ToolbarOptions/ExportMenu/PushGHDialogLogin.tsx
@@ -1,10 +1,14 @@
 import { Box, Button } from '@mui/material'
+import { loginUser } from '../../../../store/authSlice'
+import { useAppDispatch } from '../../../../store/hooks'
 
 export const PushGHDialogLogin = () => {
+    const dispatch = useAppDispatch()
+
     return (
         <Box sx={{ textAlign: 'center' }}>
             <h4>Please login to continue</h4>
-            <Button>Login to GitHub</Button>
+            <Button onClick={() => dispatch(loginUser())}>Login to GitHub</Button>
         </Box>
     )
 }
