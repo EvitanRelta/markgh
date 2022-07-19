a81335e2dab75ea9581f821e9127862f465173d8
swxk19 <s.wxk19@gmail.com>
2022-07-20 07:04:18 +0800

    Use MUI styled for 'PushGHDialogLogin'

--- src/components/Header/ToolbarOptions/ExportMenu/PushGHDialogLogin.tsx
+++ src/components/Header/ToolbarOptions/ExportMenu/PushGHDialogLogin.tsx
@@ -1,14 +1,18 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, styled } from '@mui/material'
 import { loginUser } from '../../../../store/authSlice'
 import { useAppDispatch } from '../../../../store/hooks'
+
+const StyledLoginContainer = styled(Box)({
+    textAlign: 'center',
+})
 
 export const PushGHDialogLogin = () => {
     const dispatch = useAppDispatch()
 
     return (
-        <Box sx={{ textAlign: 'center' }}>
+        <StyledLoginContainer>
             <h4>Please login to continue</h4>
             <Button onClick={() => dispatch(loginUser())}>Login to GitHub</Button>
-        </Box>
+        </StyledLoginContainer>
     )
 }
