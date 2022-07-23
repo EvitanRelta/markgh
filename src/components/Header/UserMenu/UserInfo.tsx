74e30e8e86ebdcb490d2d27798a39c596931790d
swxk19 <s.wxk19@gmail.com>
2022-07-24 01:31:38 +0800

    Change 'UserInfo' text styling

--- src/components/Header/UserMenu/UserInfo.tsx
+++ src/components/Header/UserMenu/UserInfo.tsx
@@ -1,12 +1,16 @@
-import { Box } from '@mui/material'
+import { Box, styled } from '@mui/material'
 import { useAppSelector } from '../../../store/hooks'
+
+const StyledUserInfoText = styled(Box)({
+    padding: 10,
+    fontSize: 15,
+    marginLeft: 8,
+    marginRight: 5,
+    fontWeight: 'bold',
+})
 
 export const UserInfo = () => {
     const auth = useAppSelector((state) => state.auth)
 
-    return (
-        <Box sx={{ padding: 1, fontSize: 15, marginLeft: 3 }}>
-            {auth.loggedIn && auth.user?.displayName}
-        </Box>
-    )
+    return <StyledUserInfoText>{auth.loggedIn && auth.user?.displayName}</StyledUserInfoText>
 }
