cddc36d4fed6fec927b8b4dd048ef277cfbb1929
EvitanRelta <shauntanzongzhi@gmail.com>
2022-07-24 16:39:10 +0800

    Prevent prompt for hyperlink-URL if no text selected
    Closes #195

--- src/components/Editor/toolbarFunctions/link.ts
+++ src/components/Editor/toolbarFunctions/link.ts
@@ -3,6 +3,9 @@ import { Editor } from '@tiptap/react'
 export const link = (editor: Editor | null) => () => {
     if (!editor) return
 
+    const hasNoSelection = editor.view.state.selection.empty
+    if (hasNoSelection) return
+
     const hasLink = editor.getAttributes('link').href !== undefined
     if (hasLink) {
         editor.chain().focus().unsetLink().run()
