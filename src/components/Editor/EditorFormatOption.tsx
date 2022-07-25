a44aa4ab689121e19321eaead2f8ecad3ffe15fd
swxk19 <s.wxk19@gmail.com>
2022-07-25 11:49:36 +0800

    Highlight format option scans for editor state instead of onClick

--- src/components/Editor/EditorFormatOption.tsx
+++ src/components/Editor/EditorFormatOption.tsx
@@ -1,4 +1,5 @@
 import { Box, IconButton, styled, Tooltip } from '@mui/material'
+import { Editor as CoreEditor } from '@tiptap/core'
 import { useEffect, useState } from 'react'
 import { useAppSelector } from '../../store/hooks'
 import { FormatOption } from './EditorToolbar'
@@ -29,20 +30,24 @@ export const EditorFormatOption = ({ option }: Props) => {
     const editor = useAppSelector((state) => state.data.editor)
     const [isActive, setIsActive] = useState(false)
     const [bgColor, setBgColor] = useState<string | undefined>(undefined)
-
-    const excludeHighlightActive = [
-        'Add Url Image',
-        'Horizontal Line',
-        'Ordered List',
-        'Unordered List',
-    ]
+    const attributeName = typeof option === 'object' ? option.attributeName : ''
 
     //Changes icon color if it's active, unless its excluded from activation (non-toggle-able actions)
     useEffect(() => {
-        isActive && !excludeHighlightActive.includes(option.name)
-            ? setBgColor('#3178d2')
-            : setBgColor(undefined)
+        isActive ? setBgColor('#3178d2') : setBgColor(undefined)
     }, [isActive])
+
+    useEffect(() => {
+        if (!editor) return
+
+        const getAttributeActive = (editor: CoreEditor) => {
+            return editor.isActive(attributeName)
+        }
+
+        editor.on('transaction', ({ editor }) => {
+            setIsActive(getAttributeActive(editor))
+        })
+    }, [editor])
 
     if (typeof option !== 'object') {
         const FormatOptionComponent = option
@@ -58,15 +63,10 @@ export const EditorFormatOption = ({ option }: Props) => {
             ></StyledTooltipHotkeyText>
         </StyledTooltipText>
     )
-    const handleToggleOption = () => {
-        console.log(11)
-        setIsActive(!isActive)
-        toolbarFunction(editor)()
-    }
 
     return (
         <Tooltip title={tooltipTitle} disableInteractive arrow>
-            <StyledIconButton sx={{ color: bgColor }} onClick={handleToggleOption}>
+            <StyledIconButton sx={{ color: bgColor }} onClick={toolbarFunction(editor)}>
                 <FormatOptionIcon />
             </StyledIconButton>
         </Tooltip>
